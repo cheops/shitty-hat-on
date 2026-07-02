9971738be7f4ee97773d4665a8d192a6016bc698
Joram Agten <joram.agten@gmail.com>
2026-07-03 01:40:50 +0200

    fit more patterns

--- src/main.cpp
+++ src/main.cpp
@@ -17,10 +17,7 @@
 // CRGB leds[NUM_LEDS];
 CRGBArray<NUM_LEDS> leds;
 
-CRGBSet eyes(leds(4, 5));
-CRGBSet logo(leds(8, 9));
-
-void FillLEDsFromPaletteColors(CRGBPalette16 currentPalette, TBlendType currentBlending);
+void FillLEDsFromPaletteColors(const TProgmemRGBPalette16 &currentPalette, TBlendType currentBlending);
 
 void rainbowColors();
 void partyColors();
@@ -34,9 +31,12 @@ void christmasSparklesBP();
 void heart_beat_all();
 void heart_beat_all_reverse();
 void heart_beat_eyes_red();
+void heart_beat_logo();
 void heart_beat_eyes_blue();
 void heart_beat_eyes_mono();
-void heart_beat_logo();
+
+void sinelon();
+void side_flip_flop();
 
 void heart_beat(CRGBSet lubs, CRGBSet dubs, uint8_t hue, bool change_color);
 
@@ -50,11 +50,10 @@ void setup()
     CLKPR = (1 << CLKPCE);
     CLKPR = 0;
 
-    // delay( 3000 ); // power-up safety delay // does not seem necessary, start showing patterns sooner.
-    FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
-    FastLED.setBrightness(BRIGHTNESS);
-
-    FastLED.clear(true);
+    FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS);
+#if BRIGHTNESS != 255
+    FastLED.setBrightness(BRIGHTNESS); // default scale is already 255, skip when full
+#endif
 
     // Enable global interrupts
     sei();
@@ -71,17 +70,28 @@ SimplePatternList gPatterns =
     christmasSparkles,
     partyColors,
     heart_beat_all,
-    // oceanColors,
-    // forestColors,
-    // christmasSparklesRG,
-    // christmasSparklesBP,
-    // heart_beat_all_reverse,
-    // heart_beat_eyes_blue, 
-    // heart_beat_eyes_mono, 
-    // heart_beat_logo
+    oceanColors,
+    forestColors,
+    christmasSparklesRG,
+    christmasSparklesBP,
+    heart_beat_all_reverse,
+    heart_beat_logo,
+    heart_beat_eyes_blue,
+    heart_beat_eyes_mono,
+    sinelon,
+    side_flip_flop,
 };
 
 uint8_t gCurrentPatternNumber = 0; // Index number of which pattern is current
+
+// Push the leds[] buffer to the strip with the IR pin-change interrupt disabled
+// during transmission (an ISR mid-transmission corrupts WS2812 timing).
+static void showSafe()
+{
+    PCMSK &= ~0b00010000; // disable PCINT4
+    FastLED.show();
+    PCMSK |= 0b00010000;  // re-enable PCINT4
+}
 
 //---------------------------------------------------------------
 void loop()
@@ -92,17 +102,20 @@ void loop()
     // Call the current pattern function once, updating the 'leds' array
     gPatterns[gCurrentPatternNumber]();
     
-    FastLED.show();
+    showSafe();
     // slows the framerate to a modest value
     delay(1000 / FRAMES_PER_SECOND);  // use delay, to give some time to process ir interrupts
     // FastLED.delay(1000 / FRAMES_PER_SECOND); 
 
-    // do some periodic updates
-    EVERY_N_SECONDS(20)
+    // change patterns periodically (frame-counted to avoid EVERY_N_SECONDS' 32-bit
+    // divide-by-1000, which is costly in flash on AVR)
+    static uint16_t patternFrames = 0;
+    if (++patternFrames >= FRAMES_PER_SECOND * 20) // ~20 s
     {
+        patternFrames = 0;
         nextPattern();
         FastLED.clear();
-    } // change patterns periodically
+    }
 }
 
 //---------------------------------------------------------------
@@ -110,66 +123,40 @@ void loop()
 void nextPattern()
 {
     // add one to the current pattern number, and wrap around at the end
-    gCurrentPatternNumber = (gCurrentPatternNumber + 1) % ARRAY_SIZE(gPatterns);
+    if (++gCurrentPatternNumber >= ARRAY_SIZE(gPatterns))
+        gCurrentPatternNumber = 0;
 }
 
 void handle_ir_packet(IrDataPacket packet)
 {
     if (packet.raw != 0 && packet.action == eActionDamage)
     {
-        CRGB color = CRGB::Black;
-
-        switch (packet.team)
-        {
-        case eTeamRex:
-            color = CRGB::Red;
-            break;
-
-        case eTeamBuzz:
-            color = CRGB::Blue;
-            break;
-
-        case eTeamGiggle:
-            color = CRGB::Green;
-            break;
-
-        // case eTeamYellow:
-        //     color = CRGB::Yellow;
-        //     break;
-
-        // case eTeamMagenta:
-        //     color = CRGB::Magenta;
-        //     break;
-
-        // case eTeamCyan:
-        //     color = CRGB::Cyan;
-        //     break;
-
-        // case eTeamWhite:
-        //     color = CRGB::White;
-        //     break;
-
-        default:
-            color = CRGB::Black;
-            break;
-        }
+        // Map team bits to color: R=red, G=green, B=blue (matches team bit positions)
+        CRGB color = CRGB(
+            (packet.team & eTeamRex)    ? 255 : 0,
+            (packet.team & eTeamGiggle) ? 255 : 0,
+            (packet.team & eTeamBuzz)   ? 255 : 0
+        );
 
         leds = color;
-        FastLED.show();
+        showSafe();
 
         while (leds[0].getAverageLight() != 0)
         {
             delay(1000/FRAMES_PER_SECOND);
             leds.fadeToBlackBy(10);
-            FastLED.show();
+            showSafe();
         }
-        delay(100);
         Data.readIr(); // clear buffer
         FastLED.clear();
     }
 }
 
-void FillLEDsFromPaletteColors(CRGBPalette16 currentPalette, TBlendType currentBlending)
+// Read the palette straight from PROGMEM. Taking a CRGBPalette16 here (or a
+// local copy in palettePattern) would put a 48-byte palette on the stack, and
+// with only ~115 bytes of free RAM the IR pin-change ISR nesting on top of it
+// overflows the stack into leds[], corrupting LEDs and causing flicker.
+void FillLEDsFromPaletteColors(const TProgmemRGBPalette16 &currentPalette, TBlendType currentBlending)
 {
 
     static uint8_t startIndex = 0;
@@ -184,10 +171,25 @@ void FillLEDsFromPaletteColors(CRGBPalette16 currentPalette, TBlendType currentB
     }
 }
 
-void rainbowColors() { FillLEDsFromPaletteColors(RainbowColors_p, LINEARBLEND); }
-void partyColors()   { FillLEDsFromPaletteColors(PartyColors_p,   LINEARBLEND); }
-void oceanColors()   { FillLEDsFromPaletteColors(OceanColors_p,   LINEARBLEND); }
-void forestColors()  { FillLEDsFromPaletteColors(ForestColors_p,  LINEARBLEND); }
+void palettePattern(uint8_t index);
+
+void rainbowColors() { palettePattern(0); }
+void partyColors()   { palettePattern(1); }
+void oceanColors()   { palettePattern(2); }
+void forestColors()  { palettePattern(3); }
+
+const TProgmemRGBPalette16* const paletteList[] PROGMEM = {
+    &RainbowColors_p,
+    &PartyColors_p,
+    &OceanColors_p,
+    &ForestColors_p,
+};
+
+void palettePattern(uint8_t index)
+{
+    const TProgmemRGBPalette16* pal_ptr = (const TProgmemRGBPalette16*)pgm_read_word(&paletteList[index]);
+    FillLEDsFromPaletteColors(*pal_ptr, LINEARBLEND);
+}
 
 
 
@@ -198,181 +200,115 @@ void forestColors()  { FillLEDsFromPaletteColors(ForestColors_p,  LINEARBLEND); 
 uint8_t ledsData[NUM_LEDS][4];    // for Sparkles, array to store RGB data and an extra value
 uint8_t pick;                     // for Sparkles, stores a temporary pixel number
 
-void christmasSparkles()
+// Sparkle color definitions stored in PROGMEM {hue, saturation, value}
+// All sparkle colors in one contiguous PROGMEM array
+const uint8_t PROGMEM sparkleAllColors[][3] = {
+    // Christmas multi-color (index 0-4)
+    {178, 244, 210}, // blue
+    { 10, 255, 240}, // red
+    {  0,  25, 255}, // white-ish
+    { 35, 235, 245}, // orange
+    {190, 255, 238}, // purple
+    // Red and Green (index 5-6)
+    { 16, 253, 242}, // red
+    { 96, 230, 255}, // green
+    // Blues and Purple (index 7-9)
+    {165, 180, 230}, // blue
+    {200, 170, 240}, // pink-light-purple
+    {130, 200, 255}, // light blue
+};
+
+// Sparkle preset config: bgH, bgS, bgV, chance, life, fadeRate, colorOffset, numColors
+struct SparklePreset { uint8_t bgH, bgS, bgV, chance, life, fadeRate, colorOfs, numColors; };
+const SparklePreset PROGMEM sparklePresets[] = {
+    { 50,  30,  40,  60, 35, 245, 0, 5}, // Christmas
+    {  0,   0,   0, 110, 65, 253, 5, 2}, // Red-Green
+    { 96, 185,  30, 170, 20, 242, 7, 3}, // Blues-Purple
+};
+
+void sparkles(const SparklePreset* preset_p)
 {
-    //"Background" color for non-sparkling pixels.
-    CRGB sparkleBgColor = CHSV(50, 30, 40); // dim white
-    // CRGB sparkleBgColor = CHSV(96, 200, 30);  // dim green
+    SparklePreset p;
+    memcpy_P(&p, preset_p, sizeof(p));
 
     EVERY_N_MILLISECONDS(40)
     {
-        if (random8() < 60)
-        { // How much to sparkle!  Higher number is more.
+        if (random8() < p.chance)
+        {
             pick = random8(NUM_LEDS);
             if (ledsData[pick][3] == 0)
             {
-                ledsData[pick][3] = 35; // Used to tag pixel as sparkling
-                uint8_t randomPick = random8(5);
-                if (randomPick == 0)
-                {
-                    ledsData[pick][0] = 178; // sparkle hue (blue)
-                    ledsData[pick][1] = 244; // sparkle saturation
-                    ledsData[pick][2] = 210; // sparkle value
-                }
-                if (randomPick == 1)
-                {
-                    ledsData[pick][0] = 10;  // sparkle hue (red)
-                    ledsData[pick][1] = 255; // sparkle saturation
-                    ledsData[pick][2] = 240; // sparkle value
-                }
-                if (randomPick == 2)
-                {
-                    ledsData[pick][0] = 0;   // sparkle hue (white-ish)
-                    ledsData[pick][1] = 25;  // sparkle saturation
-                    ledsData[pick][2] = 255; // sparkle value
-                }
-                if (randomPick == 3)
-                {
-                    ledsData[pick][0] = 35;  // sparkle hue (orange)
-                    ledsData[pick][1] = 235; // sparkle saturation
-                    ledsData[pick][2] = 245; // sparkle value
-                }
-                if (randomPick == 4)
-                {
-                    ledsData[pick][0] = 190; // sparkle hue (purple)
-                    ledsData[pick][1] = 255; // sparkle saturation
-                    ledsData[pick][2] = 238; // sparkle value
-                }
+                ledsData[pick][3] = p.life;
+                uint8_t idx = p.colorOfs + random8(p.numColors);
+                ledsData[pick][0] = pgm_read_byte(&sparkleAllColors[idx][0]);
+                ledsData[pick][1] = pgm_read_byte(&sparkleAllColors[idx][1]);
+                ledsData[pick][2] = pgm_read_byte(&sparkleAllColors[idx][2]);
                 leds[pick] = CHSV(ledsData[pick][0], ledsData[pick][1], ledsData[pick][2]);
             }
         }
         for (uint8_t i = 0; i < NUM_LEDS; i++)
         {
             if (ledsData[i][3] == 0)
-            { // if not sparkling, set to "back ground" color
-                leds[i] = sparkleBgColor;
+            {
+                leds[i] = CHSV(p.bgH, p.bgS, p.bgV);
             }
             else
             {
-                CHSV hsv = rgb2hsv_approximate(leds[i]);                   // Used to get approximate Hue
-                EVERY_N_MILLISECONDS(38) { ledsData[i][0] = hsv.hue - 1; } // slightly shift hue
-                ledsData[i][2] = scale8(ledsData[i][2], 245);              // slowly darken
+                EVERY_N_MILLISECONDS(38) { ledsData[i][0] = ledsData[i][0] - 1; }
+                ledsData[i][2] = scale8(ledsData[i][2], p.fadeRate);
                 leds[i] = CHSV(ledsData[i][0], ledsData[i][1], ledsData[i][2]);
-                ledsData[i][3] = ledsData[i][3] - 1; // countdown sparkle tag
+                ledsData[i][3] = ledsData[i][3] - 1;
             }
         }
     }
-} // end christmasSparkles
+}
 
-//---------------------------------------------------------------
-void christmasSparklesRG()
-{ // Red and Green only
-    //"Background" color for non-sparkling pixels.  Can be set to black for no bg color.
-    CRGB sparkleBgColor = CHSV(0, 0, 0); // black
-    // CRGB sparkleBgColor = CHSV(50, 30, 30);  // dim white
-
-    EVERY_N_MILLISECONDS(40)
-    {
-        if (random8() < 110)
-        { // How much to sparkle!  Higher number is more.
-            pick = random8(NUM_LEDS);
-            if (ledsData[pick][3] == 0)
-            {
-                ledsData[pick][3] = 65; // Used to tag pixel as sparkling
-                uint8_t randomPick = random8(2);
-                if (randomPick == 0)
-                {
-                    ledsData[pick][0] = 16;  // sparkle hue (red)
-                    ledsData[pick][1] = 253; // sparkle saturation
-                    ledsData[pick][2] = 242; // sparkle value
-                }
-                if (randomPick == 1)
-                {
-                    ledsData[pick][0] = 96;  // sparkle hue (green)
-                    ledsData[pick][1] = 230; // sparkle saturation
-                    ledsData[pick][2] = 255; // sparkle value
-                }
-                leds[pick] = CHSV(ledsData[pick][0], ledsData[pick][1], ledsData[pick][2]);
-            }
-        }
-        for (uint8_t i = 0; i < NUM_LEDS; i++)
-        {
-            if (ledsData[i][3] == 0)
-            { // if not sparkling, set to "back ground" color
-                leds[i] = sparkleBgColor;
-            }
-            else
-            {
-                CHSV hsv = rgb2hsv_approximate(leds[i]);                   // Used to get approximate Hue
-                EVERY_N_MILLISECONDS(50) { ledsData[i][0] = hsv.hue - 1; } // slightly shift hue
-                ledsData[i][2] = scale8(ledsData[i][2], 253);              // slowly darken
-                leds[i] = CHSV(ledsData[i][0], ledsData[i][1], ledsData[i][2]);
-                ledsData[i][3] = ledsData[i][3] - 1; // countdown sparkle tag
-            }
-        }
-    }
-} // end christmasSparklesRG
-
-//---------------------------------------------------------------
-void christmasSparklesBP()
-{ // Blues and Purple only
-    //"Background" color for non-sparkling pixels.
-    CRGB sparkleBgColor = CHSV(96, 185, 30); // green
-
-    EVERY_N_MILLISECONDS(40)
-    {
-        if (random8() < 170)
-        { // How much to sparkle!  Higher number is more.
-            pick = random8(NUM_LEDS);
-            if (ledsData[pick][3] == 0)
-            {
-                ledsData[pick][3] = 20; // Used to tag pixel as sparkling
-                uint8_t randomPick = random8(3);
-                if (randomPick == 0)
-                {
-                    ledsData[pick][0] = 165; // sparkle hue (blue)
-                    ledsData[pick][1] = 180; // sparkle saturation
-                    ledsData[pick][2] = 230; // sparkle value
-                }
-                if (randomPick == 1)
-                {
-                    ledsData[pick][0] = 200; // sparkle hue (pink-light-purple)
-                    ledsData[pick][1] = 170; // sparkle saturation
-                    ledsData[pick][2] = 240; // sparkle value
-                }
-                if (randomPick == 2)
-                {
-                    ledsData[pick][0] = 130; // sparkle hue (light blue)
-                    ledsData[pick][1] = 200; // sparkle saturation
-                    ledsData[pick][2] = 255; // sparkle value
-                }
-                leds[pick] = CHSV(ledsData[pick][0], ledsData[pick][1], ledsData[pick][2]);
-            }
-        }
-        for (uint8_t i = 0; i < NUM_LEDS; i++)
-        {
-            if (ledsData[i][3] == 0)
-            { // if not sparkling, set to "back ground" color
-                leds[i] = sparkleBgColor;
-            }
-            else
-            {
-                CHSV hsv = rgb2hsv_approximate(leds[i]);                   // Used to get approximate Hue
-                EVERY_N_MILLISECONDS(20) { ledsData[i][0] = hsv.hue - 1; } // slightly shift hue
-                ledsData[i][2] = scale8(ledsData[i][2], 242);              // slowly darken
-                leds[i] = CHSV(ledsData[i][0], ledsData[i][1], ledsData[i][2]);
-                ledsData[i][3] = ledsData[i][3] - 1; // countdown sparkle tag
-            }
-        }
-    }
-} // end christmasSparklesBP
+void christmasSparkles()   { sparkles(&sparklePresets[0]); }
+void christmasSparklesRG() { sparkles(&sparklePresets[1]); }
+void christmasSparklesBP() { sparkles(&sparklePresets[2]); }
 
 void heart_beat_all()         { heart_beat(leds(0, 4), leds(5, 9), HUE_RED , true) ; }
 void heart_beat_all_reverse() { heart_beat(leds(9, 5), leds(4, 0), HUE_RED , true ); }
 void heart_beat_eyes_red()    { heart_beat(leds(4, 4), leds(5, 5), HUE_RED , false); }
+void heart_beat_logo()        { heart_beat(leds(8, 8), leds(9, 9), HUE_RED , true ); }
 void heart_beat_eyes_blue()   { heart_beat(leds(4, 4), leds(5, 5), HUE_BLUE, false); }
 void heart_beat_eyes_mono()   { heart_beat(leds(4, 5), leds(4, 5), HUE_RED , false); }
-void heart_beat_logo()        { heart_beat(leds(8, 8), leds(9, 9), HUE_RED , true ); }
+
+// A colored dot sweeping back and forth with a fading trail.
+// Frame-counted (loop runs at ~60fps): no EVERY_N_MILLISECONDS objects/guards and
+// no millis() math, so it stays cheap on both RAM and flash.
+void sinelon()
+{
+    static uint8_t hue = 0, pos = 0, tick = 0;
+    static int8_t dir = 1;
+    leds.fadeToBlackBy(20);
+    if (++tick >= 4) // advance ~every 4 frames (~64 ms)
+    {
+        tick = 0;
+        hue += 3;
+        pos += dir;
+        if (pos == 0 || pos == NUM_LEDS - 1)
+            dir = -dir;
+    }
+    leds[pos] += CHSV(hue, 255, 192);
+}
+
+// Alternate the two sides of the hat (0-4 vs 5-9), crossfading, hue shifts per flip.
+void side_flip_flop()
+{
+    static uint8_t tick = 0, side = 0, hue = 0;
+    leds.fadeToBlackBy(40);
+    CRGB c = CHSV(hue, 255, 255);
+    uint8_t start = side ? 5 : 0;
+    for (uint8_t i = 0; i < 5; i++)
+        leds[start + i] = c;
+    if (++tick >= 20) // flip ~every 20 frames (~0.33 s)
+    {
+        tick = 0;
+        side ^= 1;
+        hue += 32;
+    }
+}
 
 //---------------------------------------------------------------
 // Heart beat function
